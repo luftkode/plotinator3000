22b13a90228b1c41614c9e6da91bc5f0159dd271
Marc Beck König <mbkj@tutamail.com>
2024-09-08 20:47:45 +0200

    comment

--- src/util.rs
+++ src/util.rs
@@ -1,7 +1,8 @@
 use std::time::Duration;
 
-pub fn parse_timestamp(timestamp: u32) -> String {
-    let duration = Duration::from_millis(timestamp as u64);
+/// Parse a timestamp in milliseconds to a timestamp string on the form `HH:MM:SS.zzz` where `zzz` is the millisecond fraction.
+pub fn parse_timestamp(timestamp_ms: u32) -> String {
+    let duration = Duration::from_millis(timestamp_ms as u64);
     let hours = (duration.as_secs() % 86400) / 3600;
     let minutes = (duration.as_secs() % 3600) / 60;
     let seconds = duration.as_secs() % 60;
