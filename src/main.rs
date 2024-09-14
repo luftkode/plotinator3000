65989a18b07a5db9218d788629edabb6dfd1339c
Marc Beck König <mbkj@tutamail.com>
2024-09-14 18:26:47 +0200

    fix lint

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ fn main() -> eframe::Result {
 #[cfg(target_arch = "wasm32")]
 fn main() {
     // Redirect `log` message to `console.log` and friends:
-    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+    _ = eframe::WebLogger::init(log::LevelFilter::Debug).ok();
 
     let web_options = eframe::WebOptions::default();
 
