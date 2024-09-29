f62f0b4cba9a4c6617a55b9119955f251804dd55
Marc Beck König <mbkj@tutamail.com>
2024-09-29 14:32:28 +0200

    fix copy paste errors

--- src/main.rs
+++ src/main.rs
@@ -51,7 +51,7 @@ fn main() {
             .start(
                 canvas,
                 web_options,
-                Box::new(|cc| Ok(Box::new(eframe_template::TemplateApp::new(cc)))),
+                Box::new(|cc| Ok(Box::new(logviewer_rs::App::new(cc)))),
             )
             .await;
 
