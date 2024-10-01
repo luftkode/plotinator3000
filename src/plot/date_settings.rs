395176b88ac92e987b51a9dffba1b719f8a9292a
Marc Beck König <mbkj@tutamail.com>
2024-10-01 18:50:14 +0200

    add apply_offset_to_plot_labels to also make labels affected by date offsetting

--- src/plot/date_settings.rs
+++ src/plot/date_settings.rs
@@ -1,5 +1,5 @@
 use chrono::{DateTime, NaiveDateTime, Utc};
-use plot_util::PlotWithName;
+use plot_util::{PlotWithName, StoredPlotLabels};
 use serde::{Deserialize, Serialize};
 
 #[derive(PartialEq, Eq, Deserialize, Serialize)]
@@ -29,20 +29,49 @@ impl LogStartDateSettings {
     }
 }
 
+#[allow(clippy::too_many_arguments)]
 pub fn update_plot_dates(
     invalidate_plot: &mut bool,
     percentage_plots: &mut [PlotWithName],
+    percentage_plot_labels: &mut [StoredPlotLabels],
     to_hundreds_plots: &mut [PlotWithName],
+    to_hundreds_plot_labels: &mut [StoredPlotLabels],
     to_thousands_plots: &mut [PlotWithName],
+    to_thousands_plot_labels: &mut [StoredPlotLabels],
     settings: &mut LogStartDateSettings,
 ) {
     if settings.date_changed {
         apply_offset_to_plots(percentage_plots.iter_mut(), settings);
+        apply_offset_to_plot_labels(percentage_plot_labels.iter_mut(), settings);
         apply_offset_to_plots(to_hundreds_plots.iter_mut(), settings);
+        apply_offset_to_plot_labels(to_hundreds_plot_labels.iter_mut(), settings);
         apply_offset_to_plots(to_thousands_plots.iter_mut(), settings);
+        apply_offset_to_plot_labels(to_thousands_plot_labels.iter_mut(), settings);
 
         settings.date_changed = false;
         *invalidate_plot = true;
+    }
+}
+
+fn apply_offset_to_plots<'a, I>(plots: I, settings: &LogStartDateSettings)
+where
+    I: IntoIterator<Item = &'a mut PlotWithName>,
+{
+    for plot in plots {
+        if plot.log_id == settings.log_id {
+            offset_plot(plot, settings.start_date);
+        }
+    }
+}
+
+fn apply_offset_to_plot_labels<'a, I>(stored_plot_labels: I, settings: &LogStartDateSettings)
+where
+    I: IntoIterator<Item = &'a mut StoredPlotLabels>,
+{
+    for plot_label in stored_plot_labels {
+        if plot_label.log_id() == settings.log_id {
+            offset_plot_labels(plot_label, settings.start_date);
+        }
     }
 }
 
@@ -63,13 +92,15 @@ fn offset_plot(plot: &mut PlotWithName, new_start_date: DateTime<Utc>) {
     }
 }
 
-fn apply_offset_to_plots<'a, I>(plots: I, settings: &LogStartDateSettings)
-where
-    I: IntoIterator<Item = &'a mut PlotWithName>,
-{
-    for plot in plots {
-        if plot.log_id == settings.log_id {
-            offset_plot(plot, settings.start_date);
+fn offset_plot_labels(plot_labels: &mut StoredPlotLabels, new_start_date: DateTime<Utc>) {
+    if let Some((first_point, _)) = plot_labels.label_points().first() {
+        let first_point_date = first_point[0];
+        let new_date_ns = new_start_date
+            .timestamp_nanos_opt()
+            .expect("Nanoseconds overflow") as f64;
+        let offset = new_date_ns - first_point_date;
+        for (point, _) in &mut plot_labels.label_points {
+            point[0] += offset;
         }
     }
 }
