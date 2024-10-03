7402b3c71486a1f5df7918831503a6c4b5a0c97d
Marc Beck König <mbkj@tutamail.com>
2024-10-03 09:43:55 +0200

    also show plot name on hover label

--- src/util.rs
+++ src/util.rs
@@ -34,14 +34,14 @@ pub fn format_time(mark: GridMark, _range: &RangeInclusive<f64>) -> String {
 const NANOS_PER_SEC: u64 = 1_000_000_000;
 
 /// Assumes x is time in nanoseconds
-pub fn format_label_ns(_s: &str, val: &PlotPoint) -> String {
+pub fn format_label_ns(plot_name: &str, val: &PlotPoint) -> String {
     let time_ns = val.x;
     let time_s = time_ns / NANOS_PER_SEC as f64;
     let remainder_ns = time_s.fract() * NANOS_PER_SEC as f64;
     let dt = DateTime::from_timestamp(time_s as i64, remainder_ns as u32)
         .unwrap_or_else(|| panic!("Timestamp value out of range: {}", val.x));
     format!(
-        "y: {y:.4}\n{h:02}:{m:02}:{s:02}.{subsec_ms:03}",
+        "{plot_name}\ny: {y:.4}\n{h:02}:{m:02}:{s:02}.{subsec_ms:03}",
         y = val.y,
         h = dt.hour(),
         m = dt.minute(),
