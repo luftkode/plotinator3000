f6aea130783047fbe8b7049bc1738bca2617bab4
Marc Beck König <mbkj@tutamail.com>
2024-10-16 16:13:44 +0200

    add skytem_hdf support crate

--- crates/skytem_hdf/src/lib.rs
+++ crates/skytem_hdf/src/lib.rs
@@ -1,53 +1,21 @@
+#[cfg(not(target_arch = "wasm32"))]
+pub(crate) mod util;
+
+#[cfg(not(target_arch = "wasm32"))]
 pub mod bifrost;
-pub mod util;
 
-use std::{
-    fmt::{self, Display},
-    io,
-    ops::{Add, Div},
-};
+// File extensions we recognize as hdf files.
+const POSSIBLE_HDF_EXTENSIONS_CASE_INSENSITIVE: [&str; 3] = ["h5", "hdf", "hdf5"];
 
-use hdf5::{Dataset, H5Type};
-use ndarray::{Array1, Array2, Axis};
-use num_traits::{real::Real, Bounded, FromPrimitive};
-use serde::Serialize;
+pub fn path_has_hdf_extension(path: &std::path::Path) -> bool {
+    let Some(extension) = path.extension() else {
+        return false;
+    };
 
-fn read_and_process_dataset_floats<T>(
-    dataset: &Dataset,
-    axis: usize,
-    nth_sample: usize,
-) -> io::Result<()>
-where
-    T: H5Type
-        + Serialize
-        + num_traits::identities::Zero
-        + FromPrimitive
-        + Clone
-        + Copy
-        + std::ops::Div
-        + Bounded
-        + PartialOrd
-        + Display
-        + Real
-        + for<'a> std::iter::Sum<&'a T>,
-    for<'a> &'a T: Add<T, Output = T>,
-    <T as Div>::Output: fmt::Display,
-{
-    let ndims = dataset.ndim();
-
-    match ndims {
-        1 => {
-            // Read the dataset into a 1D ndarray
-            let data_1dim: Array1<T> = dataset.read_1d()?;
-        }
-        2 => {
-            // Read the dataset into a 2D ndarray
-            let data_2dim: Array2<T> = dataset.read_2d()?;
-            let data_2dim_folded = data_2dim.fold_axis(Axis(axis), T::zero(), |acc, &x| acc + x);
-        }
-        _ => {
-            todo!("Unsupported dataset dimensionality: {ndims}");
+    for possible_extension in POSSIBLE_HDF_EXTENSIONS_CASE_INSENSITIVE {
+        if extension.eq_ignore_ascii_case(possible_extension) {
+            return true;
         }
     }
-    Ok(())
+    false
 }
