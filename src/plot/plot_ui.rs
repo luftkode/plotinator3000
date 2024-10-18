c875e5de42e7fc83201df1a0698127cafe014dbb
Marc Beck König <mbkj@tutamail.com>
2024-10-18 23:42:41 +0200

    make plot settings wrap

--- src/plot/plot_ui.rs
+++ src/plot/plot_ui.rs
@@ -10,32 +10,30 @@ pub fn show_settings_grid(
     axis_cfg: &mut AxisConfig,
     plot_settings: &mut PlotSettings,
 ) {
-    egui::Grid::new("settings").show(ui, |ui| {
-        ui.horizontal_top(|ui| {
-            plot_settings.show(ui);
-            ui.label("|");
-            let axis_cfg_str = RichText::new(format!("{} Axis config", regular::GEAR));
-            if ui.button(axis_cfg_str.clone()).clicked() {
-                axis_cfg.ui_visible = !axis_cfg.ui_visible;
-            }
-            if axis_cfg.ui_visible {
-                let mut open: bool = axis_cfg.ui_visible;
-                egui::Window::new(axis_cfg_str)
-                    .open(&mut open)
-                    .show(ui.ctx(), |ui| {
-                        axis_cfg.toggle_axis_cfg_ui(ui);
-                    });
-                axis_cfg.ui_visible = open;
-            }
-            if ui.ctx().input(|i| i.key_pressed(Key::Escape)) {
-                axis_cfg.ui_visible = false;
-            }
-            ui.label("Line width");
-            ui.add(
-                egui::DragValue::new(line_width)
-                    .speed(0.02)
-                    .range(0.5..=20.0),
-            );
-        });
+    ui.horizontal_wrapped(|ui| {
+        plot_settings.show(ui);
+        ui.label("|");
+        let axis_cfg_str = RichText::new(format!("{} Axis config", regular::GEAR));
+        if ui.button(axis_cfg_str.clone()).clicked() {
+            axis_cfg.ui_visible = !axis_cfg.ui_visible;
+        }
+        if axis_cfg.ui_visible {
+            let mut open: bool = axis_cfg.ui_visible;
+            egui::Window::new(axis_cfg_str)
+                .open(&mut open)
+                .show(ui.ctx(), |ui| {
+                    axis_cfg.toggle_axis_cfg_ui(ui);
+                });
+            axis_cfg.ui_visible = open;
+        }
+        if ui.ctx().input(|i| i.key_pressed(Key::Escape)) {
+            axis_cfg.ui_visible = false;
+        }
+        ui.label("Line width");
+        ui.add(
+            egui::DragValue::new(line_width)
+                .speed(0.02)
+                .range(0.5..=20.0),
+        );
     });
 }
