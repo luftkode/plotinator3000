14ca93eba3e8336125914ee9c9efbb829c603ccb
Marc Beck König <mbkj@tutamail.com>
2024-10-21 11:37:22 +0200

    build the plots

--- crates/skytem_logs/src/navsys.rs
+++ crates/skytem_logs/src/navsys.rs
@@ -16,6 +16,210 @@ mod header;
 pub struct NavSysSps {
     header: NavSysSpsHeader,
     entries: Vec<NavSysSpsEntry>,
+    raw_plots: Vec<RawPlot>,
+}
+
+impl NavSysSps {
+    fn build_raw_plots(entries: &[NavSysSpsEntry]) -> Vec<RawPlot> {
+        let mut raw_he1_points_altitude: Vec<[f64; 2]> = Vec::new();
+        let mut raw_he2_points_altitude: Vec<[f64; 2]> = Vec::new();
+        let mut raw_tl1_points_pitch: Vec<[f64; 2]> = Vec::new();
+        let mut raw_tl2_points_pitch: Vec<[f64; 2]> = Vec::new();
+        let mut raw_tl1_points_roll: Vec<[f64; 2]> = Vec::new();
+        let mut raw_tl2_points_roll: Vec<[f64; 2]> = Vec::new();
+        let mut raw_gp1_points_latitude: Vec<[f64; 2]> = Vec::new();
+        let mut raw_gp2_points_latitude: Vec<[f64; 2]> = Vec::new();
+        let mut raw_gp1_points_longitude: Vec<[f64; 2]> = Vec::new();
+        let mut raw_gp2_points_longitude: Vec<[f64; 2]> = Vec::new();
+        let mut raw_gp1_points_gps_time_delta_ms: Vec<[f64; 2]> = Vec::new();
+        let mut raw_gp2_points_gps_time_delta_ms: Vec<[f64; 2]> = Vec::new();
+        let mut raw_gp1_points_num_satellites: Vec<[f64; 2]> = Vec::new();
+        let mut raw_gp2_points_num_satellites: Vec<[f64; 2]> = Vec::new();
+        let mut raw_gp1_points_speed_kmh: Vec<[f64; 2]> = Vec::new();
+        let mut raw_gp2_points_speed_kmh: Vec<[f64; 2]> = Vec::new();
+        let mut raw_gp1_points_hdop: Vec<[f64; 2]> = Vec::new();
+        let mut raw_gp2_points_hdop: Vec<[f64; 2]> = Vec::new();
+        let mut raw_gp1_points_vdop: Vec<[f64; 2]> = Vec::new();
+        let mut raw_gp2_points_vdop: Vec<[f64; 2]> = Vec::new();
+        let mut raw_gp1_points_pdop: Vec<[f64; 2]> = Vec::new();
+        let mut raw_gp2_points_pdop: Vec<[f64; 2]> = Vec::new();
+        let mut raw_gp1_points_altitude: Vec<[f64; 2]> = Vec::new();
+        let mut raw_gp2_points_altitude: Vec<[f64; 2]> = Vec::new();
+        let mut raw_mag1_points: Vec<[f64; 2]> = Vec::new();
+
+        for entry in entries {
+            match entry {
+                NavSysSpsEntry::HE1(e) => {
+                    raw_he1_points_altitude.push([e.timestamp_ns(), e.altitude_m()]);
+                }
+                NavSysSpsEntry::HE2(e) => {
+                    raw_he2_points_altitude.push([e.timestamp_ns(), e.altitude_m()]);
+                }
+                NavSysSpsEntry::TL1(e) => {
+                    raw_tl1_points_pitch.push([e.timestamp_ns(), e.pitch_angle_degrees()]);
+                    raw_tl1_points_roll.push([e.timestamp_ns(), e.roll_angle_degrees()]);
+                }
+                NavSysSpsEntry::TL2(e) => {
+                    raw_tl2_points_pitch.push([e.timestamp_ns(), e.pitch_angle_degrees()]);
+                    raw_tl2_points_roll.push([e.timestamp_ns(), e.roll_angle_degrees()]);
+                }
+                NavSysSpsEntry::GP1(e) => {
+                    let ts = e.timestamp_ns();
+                    raw_gp1_points_latitude.push([ts, e.latitude()]);
+                    raw_gp1_points_longitude.push([ts, e.longitude()]);
+                    raw_gp1_points_gps_time_delta_ms.push([ts, e.gps_time_delta_ms()]);
+                    raw_gp1_points_num_satellites.push([ts, e.num_satellites().into()]);
+                    raw_gp1_points_speed_kmh.push([ts, e.speed_kmh().into()]);
+                    raw_gp1_points_hdop.push([ts, e.hdop().into()]);
+                    raw_gp1_points_vdop.push([ts, e.vdop().into()]);
+                    raw_gp1_points_pdop.push([ts, e.pdop().into()]);
+                    raw_gp1_points_altitude.push([ts, e.altitude_above_mean_sea().into()]);
+                }
+                NavSysSpsEntry::GP2(e) => {
+                    let ts = e.timestamp_ns();
+                    raw_gp2_points_latitude.push([ts, e.latitude()]);
+                    raw_gp2_points_longitude.push([ts, e.longitude()]);
+                    raw_gp2_points_gps_time_delta_ms.push([ts, e.gps_time_delta_ms()]);
+                    raw_gp2_points_num_satellites.push([ts, e.num_satellites().into()]);
+                    raw_gp2_points_speed_kmh.push([ts, e.speed_kmh().into()]);
+                    raw_gp2_points_hdop.push([ts, e.hdop().into()]);
+                    raw_gp2_points_vdop.push([ts, e.vdop().into()]);
+                    raw_gp2_points_pdop.push([ts, e.pdop().into()]);
+                    raw_gp2_points_altitude.push([ts, e.altitude_above_mean_sea().into()]);
+                }
+                NavSysSpsEntry::MA1(e) => {
+                    raw_mag1_points.push([e.timestamp_ns(), e.field_nanotesla()]);
+                }
+            }
+        }
+        vec![
+            RawPlot::new(
+                "HE1 Altitude [M]".into(),
+                raw_he1_points_altitude,
+                ExpectedPlotRange::Thousands,
+            ),
+            RawPlot::new(
+                "HE2 Altitude [M]".into(),
+                raw_he2_points_altitude,
+                ExpectedPlotRange::Thousands,
+            ),
+            RawPlot::new(
+                "TL1 Pitch".into(),
+                raw_tl1_points_pitch,
+                ExpectedPlotRange::OneToOneHundred,
+            ),
+            RawPlot::new(
+                "TL2 Pitch".into(),
+                raw_tl2_points_pitch,
+                ExpectedPlotRange::OneToOneHundred,
+            ),
+            RawPlot::new(
+                "TL1 Roll".into(),
+                raw_tl1_points_roll,
+                ExpectedPlotRange::OneToOneHundred,
+            ),
+            RawPlot::new(
+                "TL2 Roll".into(),
+                raw_tl2_points_roll,
+                ExpectedPlotRange::OneToOneHundred,
+            ),
+            RawPlot::new(
+                "GP1 Latitude".into(),
+                raw_gp1_points_latitude,
+                ExpectedPlotRange::Thousands,
+            ),
+            RawPlot::new(
+                "GP2 Latitude".into(),
+                raw_gp2_points_latitude,
+                ExpectedPlotRange::Thousands,
+            ),
+            RawPlot::new(
+                "GP1 Longitude".into(),
+                raw_gp1_points_longitude,
+                ExpectedPlotRange::Thousands,
+            ),
+            RawPlot::new(
+                "GP2 Longitude".into(),
+                raw_gp2_points_longitude,
+                ExpectedPlotRange::Thousands,
+            ),
+            RawPlot::new(
+                "GP1 Time delta [ms]".into(),
+                raw_gp1_points_gps_time_delta_ms,
+                ExpectedPlotRange::Thousands,
+            ),
+            RawPlot::new(
+                "GP2 Time delta [ms]".into(),
+                raw_gp2_points_gps_time_delta_ms,
+                ExpectedPlotRange::Thousands,
+            ),
+            RawPlot::new(
+                "GP1 Satelittes".into(),
+                raw_gp1_points_num_satellites,
+                ExpectedPlotRange::OneToOneHundred,
+            ),
+            RawPlot::new(
+                "GP2 Satelittes".into(),
+                raw_gp2_points_num_satellites,
+                ExpectedPlotRange::OneToOneHundred,
+            ),
+            RawPlot::new(
+                "GP1 Speed [km/h]".into(),
+                raw_gp1_points_speed_kmh,
+                ExpectedPlotRange::OneToOneHundred,
+            ),
+            RawPlot::new(
+                "GP2 Speed [km/h]".into(),
+                raw_gp2_points_speed_kmh,
+                ExpectedPlotRange::OneToOneHundred,
+            ),
+            RawPlot::new(
+                "GP1 HDOP".into(),
+                raw_gp1_points_hdop,
+                ExpectedPlotRange::OneToOneHundred,
+            ),
+            RawPlot::new(
+                "GP2 HDOP".into(),
+                raw_gp2_points_hdop,
+                ExpectedPlotRange::OneToOneHundred,
+            ),
+            RawPlot::new(
+                "GP1 VDOP".into(),
+                raw_gp1_points_vdop,
+                ExpectedPlotRange::OneToOneHundred,
+            ),
+            RawPlot::new(
+                "GP2 VDOP".into(),
+                raw_gp2_points_vdop,
+                ExpectedPlotRange::OneToOneHundred,
+            ),
+            RawPlot::new(
+                "GP1 PDOP".into(),
+                raw_gp1_points_pdop,
+                ExpectedPlotRange::OneToOneHundred,
+            ),
+            RawPlot::new(
+                "GP2 PDOP".into(),
+                raw_gp2_points_pdop,
+                ExpectedPlotRange::OneToOneHundred,
+            ),
+            RawPlot::new(
+                "GP1 Altitude [m]".into(),
+                raw_gp1_points_altitude,
+                ExpectedPlotRange::OneToOneHundred,
+            ),
+            RawPlot::new(
+                "GP2 Altitude [m]".into(),
+                raw_gp2_points_altitude,
+                ExpectedPlotRange::OneToOneHundred,
+            ),
+            RawPlot::new(
+                "MA1 B-field [nT]".into(),
+                raw_mag1_points,
+                ExpectedPlotRange::Thousands,
+            ),
+        ]
+    }
 }
 
 impl fmt::Display for NavSysSps {
@@ -30,7 +234,7 @@ impl fmt::Display for NavSysSps {
 
 impl Plotable for NavSysSps {
     fn raw_plots(&self) -> &[RawPlot] {
-        todo!()
+        &self.raw_plots
     }
 
     fn first_timestamp(&self) -> DateTime<Utc> {
@@ -58,7 +262,7 @@ impl Plotable for NavSysSps {
             ),
         ];
 
-        todo!()
+        Some(metadata)
     }
 }
 
@@ -81,7 +285,16 @@ impl Parseable for NavSysSps {
         let (entries, bytes_read) = parse_to_vec(reader);
         total_bytes_read += bytes_read;
 
-        Ok((Self { header, entries }, total_bytes_read))
+        let raw_plots = Self::build_raw_plots(&entries);
+
+        Ok((
+            Self {
+                header,
+                entries,
+                raw_plots,
+            },
+            total_bytes_read,
+        ))
     }
 
     fn is_buf_valid(buf: &[u8]) -> bool {
