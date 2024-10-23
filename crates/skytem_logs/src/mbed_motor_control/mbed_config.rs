6f25956aa0c66d6a2303f1d135289474ddf999f3
Marc Beck König <mbkj@tutamail.com>
2024-10-23 14:43:38 +0200

    initial mbed_configv2

--- crates/skytem_logs/src/mbed_motor_control/mbed_config.rs
+++ crates/skytem_logs/src/mbed_motor_control/mbed_config.rs
@@ -2,6 +2,8 @@ use std::io::{self};
 
 pub(crate) mod v1;
 pub(crate) use v1::MbedConfigV1;
+pub(crate) mod v2;
+pub(crate) use v2::MbedConfigV2;
 
 pub(crate) trait MbedConfig: Sized {
     /// As long as implementors are packed structs, this can just return `size_of::<Self>()`, otherwise it should return
