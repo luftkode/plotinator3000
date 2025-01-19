1f4f340c0c0d01e593767609774bc6251dd68006
Marc Beck König <mbkj@tutamail.com>
2025-01-19 16:07:34 +0100

    add generic error window to display error to users

---
+++ src/updater/ui/error_window.rs
@@ -0,0 +1,53 @@
+use egui::{Color32, RichText};
+
+use crate::APP_NAME;
+
+#[allow(dead_code)]
+/// Display a simple window with the error that occurred
+pub(crate) fn show_error_occured(err_msg: &str) {
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_min_inner_size([300.0, 300.0])
+            .with_icon(
+                eframe::icon_data::from_png_bytes(crate::APP_ICON).expect("Failed to load icon"),
+            ),
+        ..Default::default()
+    };
+
+    eframe::run_simple_native(APP_NAME, options, {
+        let err_msg = err_msg.to_owned();
+        move |ctx, _frame| {
+            egui::CentralPanel::default().show(ctx, |ui| {
+                let err_msg = err_msg.to_owned();
+                {
+                    ui.vertical_centered(|ui| {
+                        ui.heading(RichText::new("⚠").size(30.0).color(Color32::RED));
+                        ui.add_space(10.0);
+
+                        ui.label(
+                            RichText::new(format!("An error occurred: "))
+                                .size(18.)
+                                .strong(),
+                        );
+                        ui.label(RichText::new(err_msg).strong().size(20.));
+                        ui.add_space(10.0);
+                        ui.label("Please report this error at the link below");
+                        ui.add(egui::Hyperlink::from_label_and_url(
+                            "Plotinator3000 issues",
+                            "https://github.com/luftkode/plotinator3000/issues",
+                        ));
+
+                        ui.add_space(10.0);
+                        if ui
+                            .button(RichText::new("Close").strong().size(18.0))
+                            .clicked()
+                        {
+                            ctx.send_viewport_cmd(egui::ViewportCommand::Close);
+                        }
+                    });
+                }
+            });
+        }
+    })
+    .expect("Failed launching error window");
+}
