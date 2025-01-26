0d7540039764317fcc3ce79fe93e8db516bc4b46
Marc Beck König <mbkj@tutamail.com>
2025-01-26 22:24:26 +0100

    refactor

--- src/plot/click_delta.rs
+++ src/plot/click_delta.rs
@@ -6,6 +6,7 @@ use serde::{Deserialize, Serialize};
 
 use super::PlotType;
 
+/// Keeps track of clicks in plot areas to show the delta (x and y) of two different clicks.
 #[derive(Debug, Default, Clone, Copy, PartialEq, Deserialize, Serialize)]
 pub struct ClickDelta {
     // Which plot type the click belongs to
@@ -16,48 +17,6 @@ pub struct ClickDelta {
 }
 
 impl ClickDelta {
-    fn delta_x(x1: f64, x2: f64) -> f64 {
-        (x2 - x1).abs() / 1_000_000_000.
-    }
-
-    // Calculate the x offset for the text that describes the point delta
-    fn calc_text_x_offset(x2: f64, range_x: RangeInclusive<f64>) -> f64 {
-        let plot_bounds_delta_x = (range_x.start() - range_x.end()).abs();
-        let x2_closest_right = (x2 - range_x.end()).abs() < (x2 - range_x.start()).abs();
-        if x2_closest_right {
-            -plot_bounds_delta_x / 50.
-        } else {
-            plot_bounds_delta_x / 50.
-        }
-    }
-
-    // Calculate the y offset for the text that describes the point delta
-    fn calc_text_y_offset(y2: f64, range_y: RangeInclusive<f64>) -> f64 {
-        let offset_factor = 15.;
-        let plot_bounds_delta_y = (range_y.start() - range_y.end()).abs();
-
-        let y2_closest_top = (y2 - range_y.end()).abs() < (y2 - range_y.start()).abs();
-        if y2_closest_top {
-            -plot_bounds_delta_y / offset_factor
-        } else {
-            plot_bounds_delta_y / offset_factor
-        }
-    }
-
-    pub fn get_delta_text(a: [f64; 2], b: [f64; 2], plot_bounds: PlotBounds) -> egui_plot::Text {
-        let [x1, y1] = a;
-        let [x2, y2] = b;
-        let delta_x = Self::delta_x(x1, x2);
-        let delta_y = y2 - y1;
-        let delta_formatted = crate::util::format_delta_xy(delta_x, delta_y);
-        let x_offset = Self::calc_text_x_offset(x2, plot_bounds.range_x());
-        let y_offset = Self::calc_text_y_offset(y2, plot_bounds.range_y());
-        let dist_x = x2 - x1;
-        let label_x = x1 + dist_x / 2. + x_offset;
-        let label_y = y1 + delta_y / 2. + y_offset;
-        let label_point = PlotPoint::new(label_x, label_y);
-        egui_plot::Text::new(label_point, delta_formatted).highlight(true)
-    }
 
     pub fn set_next_click(&mut self, click: PlotPoint, plot_type: PlotType) {
         if self.plot_type.is_some_and(|pt| pt == plot_type) {
@@ -77,13 +36,22 @@ impl ClickDelta {
         }
     }
 
-    fn replace_first_click(&mut self, click: [f64; 2]) {
-        self.first_click = Some(click);
+    pub fn get_delta_text(a: [f64; 2], b: [f64; 2], plot_bounds: PlotBounds) -> egui_plot::Text {
+        let [x1, y1] = a;
+        let [x2, y2] = b;
+        let delta_x = Self::delta_x(x1, x2);
+        let delta_y = y2 - y1;
+        let delta_formatted = crate::util::format_delta_xy(delta_x, delta_y);
+        let x_offset = Self::calc_text_x_offset(x2, plot_bounds.range_x());
+        let y_offset = Self::calc_text_y_offset(y2, plot_bounds.range_y());
+        let dist_x = x2 - x1;
+        let label_x = x1 + dist_x / 2. + x_offset;
+        let label_y = y1 + delta_y / 2. + y_offset;
+        let label_point = PlotPoint::new(label_x, label_y);
+        egui_plot::Text::new(label_point, delta_formatted).highlight(true)
     }
 
-    fn replace_second_click(&mut self, click: [f64; 2]) {
-        self.second_click = Some(click);
-    }
+    
 
     pub fn plot_type(&self) -> Option<PlotType> {
         self.plot_type
@@ -102,27 +70,51 @@ impl ClickDelta {
         (self.first_click, self.second_click)
     }
 
+    fn delta_x(x1: f64, x2: f64) -> f64 {
+        (x2 - x1).abs() / 1_000_000_000.
+    }
+
+    // Calculate the x offset for the text that describes the point delta
+    fn calc_text_x_offset(x2: f64, range_x: RangeInclusive<f64>) -> f64 {
+        let offset_factor = 50.;
+        let plot_bounds_delta_x = (range_x.start() - range_x.end()).abs();
+        let offset = plot_bounds_delta_x / offset_factor;
+        
+        let x2_closest_right = (x2 - range_x.end()).abs() < (x2 - range_x.start()).abs();
+        if x2_closest_right {
+            -offset
+        } else {
+            offset
+        }
+    }
+
+    // Calculate the y offset for the text that describes the point delta
+    fn calc_text_y_offset(y2: f64, range_y: RangeInclusive<f64>) -> f64 {
+        let offset_factor = 10.;
+        let plot_bounds_delta_y = (range_y.start() - range_y.end()).abs();
+        let offset = plot_bounds_delta_y / offset_factor;
+
+        let y2_closest_top = (y2 - range_y.end()).abs() < (y2 - range_y.start()).abs();
+        if y2_closest_top {
+            -offset
+        } else {
+            offset
+        }
+    }
+
+    fn replace_first_click(&mut self, click: [f64; 2]) {
+        self.first_click = Some(click);
+    }
+
+    fn replace_second_click(&mut self, click: [f64; 2]) {
+        self.second_click = Some(click);
+    }
+
     fn delta_color(plot_ui_ctx: &egui::Context) -> Color32 {
         match plot_ui_ctx.theme() {
             egui::Theme::Dark => Color32::WHITE,
             egui::Theme::Light => Color32::BLACK,
         }
-    }
-
-    fn ui_delta_line(plot_ui: &mut PlotUi, delta_line: Line, a: [f64; 2], b: [f64; 2]) {
-        let delta_text = Self::get_delta_text(a, b, plot_ui.plot_bounds());
-        plot_ui.text(delta_text);
-        plot_ui.add(delta_line);
-    }
-
-    fn ui_click_markers(plot_ui: &mut PlotUi, points: Points, color: Color32) {
-        plot_ui.add(
-            points
-                .shape(MarkerShape::Cross)
-                .highlight(true)
-                .radius(4.0)
-                .color(color),
-        );
     }
 
     pub fn ui(&self, plot_ui: &mut PlotUi, plot_type: PlotType) {
@@ -155,5 +147,21 @@ impl ClickDelta {
                 }
             }
         }
+    }
+
+    fn ui_delta_line(plot_ui: &mut PlotUi, delta_line: Line, a: [f64; 2], b: [f64; 2]) {
+        let delta_text = Self::get_delta_text(a, b, plot_ui.plot_bounds());
+        plot_ui.text(delta_text);
+        plot_ui.add(delta_line);
+    }
+
+    fn ui_click_markers(plot_ui: &mut PlotUi, points: Points, color: Color32) {
+        plot_ui.add(
+            points
+                .shape(MarkerShape::Cross)
+                .highlight(true)
+                .radius(4.0)
+                .color(color),
+        );
     }
 }
