c8662a2e58bd779d0e7a2ab8f43326abe7289f0f
Marc <72197092+CramBL@users.noreply.github.com>
2025-02-09 14:37:58 +0100

    Add profiling feature with code to ease the ability for developers … (#189)
    * Add `profiling` feature with code to ease the ability for developers to profile `plotinator3000`
    
    * remove missing_debug_implementations lint. it's just annoying
    
    * refactor to remove the missing_debug_implementations allow lints, and to reduce the number of lines in App:update

--- src/plot/plot_graphics.rs
+++ src/plot/plot_graphics.rs
@@ -30,6 +30,8 @@ pub fn paint_plots(
     line_width: f32,
     click_delta: &mut ClickDelta,
 ) {
+    #[cfg(all(feature = "profiling", not(target_arch = "wasm32")))]
+    puffin::profile_function!();
     let plot_height = ui.available_height() / (plot_settings.total_plot_count() as f32);
 
     let x_axes = vec![AxisHints::new_x().formatter(crate::util::format_time)];
@@ -108,6 +110,8 @@ fn fill_plots(
     plot_settings: &PlotSettings,
     click_delta: &mut ClickDelta,
 ) {
+    #[cfg(all(feature = "profiling", not(target_arch = "wasm32")))]
+    puffin::profile_function!();
     for (ui, plot, ptype) in plot_components {
         ui.show(gui, |plot_ui| {
             let resp = plot_ui.response();
@@ -149,6 +153,8 @@ fn fill_plot(
     line_width: f32,
     plot_settings: &PlotSettings,
 ) {
+    #[cfg(all(feature = "profiling", not(target_arch = "wasm32")))]
+    puffin::profile_function!();
     let (plot_data, plot_type) = plot;
 
     plot_util::plot_lines(
