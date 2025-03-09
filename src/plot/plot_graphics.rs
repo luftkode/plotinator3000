38b4cae23e0a5876fe8992967fff4e66427432ce
Marc Beck König <mbkj@tutamail.com>
2025-03-09 09:58:02 +0100

    use a plotmode to divide log plots and MQTT plot

--- src/plot/plot_graphics.rs
+++ src/plot/plot_graphics.rs
@@ -6,6 +6,11 @@ use crate::{mqtt::MqttData, plot::util};
 
 use super::{axis_config::AxisConfig, plot_settings::PlotSettings, ClickDelta, PlotType};
 
+enum PlotMode {
+    Logs,
+    MQTT,
+}
+
 /// Paints multiple plots based on the provided settings and configurations.
 ///
 /// # Arguments
@@ -38,81 +43,93 @@ pub fn paint_plots(
 ) {
     #[cfg(all(feature = "profiling", not(target_arch = "wasm32")))]
     puffin::profile_function!();
-    let plot_height = ui.available_height() / (plot_settings.total_plot_count() as f32);
-
+    let mode = if mqtt_plots.is_empty() {
+        PlotMode::Logs
+    } else {
+        PlotMode::MQTT
+    };
     let x_axes = vec![AxisHints::new_x().formatter(crate::util::format_time)];
 
-    let percentage_plot = build_plot_ui(
-        "percentage",
-        plot_height,
-        legend_cfg.clone(),
-        axis_cfg,
-        x_axes.clone(),
-        link_group,
-    )
-    .include_y(1.0)
-    .y_axis_formatter(|y, _range| format!("{:.0}%", y.value * 100.0));
+    match mode {
+        PlotMode::Logs => {
+            let plot_height = ui.available_height() / (plot_settings.total_plot_count() as f32);
 
-    let to_hundred_plot = build_plot_ui(
-        "to_hundred",
-        plot_height,
-        legend_cfg.clone(),
-        axis_cfg,
-        x_axes.clone(),
-        link_group,
-    );
-    let thousands_plot: Plot<'_> = build_plot_ui(
-        "thousands",
-        plot_height,
-        legend_cfg.clone(),
-        axis_cfg,
-        x_axes.clone(),
-        link_group,
-    );
-    let mut plot_components_list = Vec::with_capacity(plot_settings.total_plot_count().into());
+            let percentage_plot = build_plot_ui(
+                "percentage",
+                plot_height,
+                legend_cfg.clone(),
+                axis_cfg,
+                x_axes.clone(),
+                link_group,
+            )
+            .include_y(1.0)
+            .y_axis_formatter(|y, _range| format!("{:.0}%", y.value * 100.0));
 
-    let Plots {
-        percentage,
-        one_to_hundred,
-        thousands,
-    } = plots;
+            let to_hundred_plot = build_plot_ui(
+                "to_hundred",
+                plot_height,
+                legend_cfg.clone(),
+                axis_cfg,
+                x_axes.clone(),
+                link_group,
+            );
+            let thousands_plot: Plot<'_> = build_plot_ui(
+                "thousands",
+                plot_height,
+                legend_cfg.clone(),
+                axis_cfg,
+                x_axes.clone(),
+                link_group,
+            );
+            let mut plot_components_list =
+                Vec::with_capacity(plot_settings.total_plot_count().into());
 
-    if plot_settings.display_percentage() {
-        plot_components_list.push((percentage_plot, percentage, PlotType::Percentage));
+            let Plots {
+                percentage,
+                one_to_hundred,
+                thousands,
+            } = plots;
+
+            if plot_settings.display_percentage() {
+                plot_components_list.push((percentage_plot, percentage, PlotType::Percentage));
+            }
+
+            if plot_settings.display_hundreds() {
+                plot_components_list.push((to_hundred_plot, one_to_hundred, PlotType::Hundreds));
+            }
+
+            if plot_settings.display_thousands() {
+                plot_components_list.push((thousands_plot, thousands, PlotType::Thousands));
+            }
+            fill_log_plots(
+                ui,
+                reset_plot_bounds,
+                plot_components_list,
+                line_width,
+                plot_settings,
+                click_delta,
+            );
+        }
+        PlotMode::MQTT => {
+            let mqtt_plot = build_plot_ui(
+                "mqtt",
+                ui.available_height(),
+                legend_cfg.clone(),
+                axis_cfg,
+                x_axes,
+                link_group,
+            );
+            fill_mqtt_plots(
+                ui,
+                reset_plot_bounds,
+                line_width,
+                click_delta,
+                mqtt_plot,
+                mqtt_plots,
+                auto_scale,
+            );
+        }
     }
-
-    if plot_settings.display_hundreds() {
-        plot_components_list.push((to_hundred_plot, one_to_hundred, PlotType::Hundreds));
-    }
-
-    if plot_settings.display_thousands() {
-        plot_components_list.push((thousands_plot, thousands, PlotType::Thousands));
-    }
-
-    let mqtt_plot_area: Option<Plot<'_>> = if mqtt_plots.is_empty() {
-        None
-    } else {
-        Some(build_plot_ui(
-            "mqtt",
-            ui.available_height(),
-            legend_cfg.clone(),
-            axis_cfg,
-            x_axes,
-            link_group,
-        ))
-    };
-
-    fill_plots(
-        ui,
-        reset_plot_bounds,
-        plot_components_list,
-        line_width,
-        plot_settings,
-        click_delta,
-        mqtt_plot_area,
-        mqtt_plots,
-        auto_scale,
-    );
 }
 
 /// Iterates through and fills/paints all plots with their respective data.
@@ -125,16 +142,13 @@ pub fn paint_plots(
 /// * `line_width` - The width of plot lines.
 /// * `plot_settings` - Controls which plots to display.
 /// * `click_delta` - State relating to pointer clicks on plots
-fn fill_plots(
+fn fill_log_plots(
     gui: &mut egui::Ui,
     reset_plot_bounds: bool,
     plot_components: Vec<(Plot<'_>, &mut PlotData, PlotType)>,
     line_width: f32,
     plot_settings: &PlotSettings,
     click_delta: &mut ClickDelta,
-    mqtt_plot_area: Option<Plot<'_>>,
-    mqtt_plots: &[MqttData],
-    auto_scale: &mut bool,
 ) {
     #[cfg(all(feature = "profiling", not(target_arch = "wasm32")))]
     puffin::profile_function!();
@@ -182,59 +196,82 @@ fn fill_plots(
             fill_plot(plot_ui, plot, line_width, plot_settings);
         });
     }
-    if let Some(mqtt_plot_area) = mqtt_plot_area {
-        mqtt_plot_area.show(gui, |plot_ui| {
-            if plot_ui.response().hovered() {
-                if let Some(final_zoom_factor) = final_zoom_factor {
-                    plot_ui.zoom_bounds_around_hovered(final_zoom_factor);
-                }
+}
+
+/// Iterates through and fills/paints all plots with their respective data.
+///
+/// # Arguments
+///
+/// * `gui` - The egui UI to paint on.
+/// * `reset_plot_bounds` - whether plot bounds should be reset.
+/// * `line_width` - The width of plot lines.
+/// * `click_delta` - State relating to pointer clicks on plots
+fn fill_mqtt_plots(
+    gui: &mut egui::Ui,
+    reset_plot_bounds: bool,
+    line_width: f32,
+    click_delta: &mut ClickDelta,
+    mqtt_plot_area: Plot<'_>,
+    mqtt_plots: &[MqttData],
+    auto_scale: &mut bool,
+) {
+    #[cfg(all(feature = "profiling", not(target_arch = "wasm32")))]
+    puffin::profile_function!();
+
+    let (scroll, modifiers) = util::get_cursor_scroll_input(gui);
+    let final_zoom_factor: Option<Vec2> = scroll.and_then(|s| util::set_zoom_factor(s, modifiers));
+
+    mqtt_plot_area.show(gui, |plot_ui| {
+        if plot_ui.response().hovered() {
+            if let Some(final_zoom_factor) = final_zoom_factor {
+                plot_ui.zoom_bounds_around_hovered(final_zoom_factor);
             }
-            let resp = plot_ui.response();
-            if plot_ui.response().double_clicked() || reset_plot_bounds {
-                log::info!("Auto scaling re-enabled");
-                *auto_scale = true;
-                if let Some(max_bounds) = get_mqtt_auto_scaled_plot_bounds(mqtt_plots) {
-                    plot_ui.set_plot_bounds(max_bounds);
-                }
-            } else if resp.clicked() {
-                *auto_scale = false;
-                log::info!("Auto scaling DISABLED!");
-                if plot_ui.ctx().input(|i| i.modifiers.shift) {
-                    if let Some(pointer_coordinate) = plot_ui.pointer_coordinate() {
-                        click_delta.set_next_click(pointer_coordinate, PlotType::Hundreds);
-                    }
-                } else {
-                    click_delta.reset();
-                }
-            } else if resp.is_pointer_button_down_on() {
-                log::info!("Auto scaling DISABLED!");
-                *auto_scale = false;
-            } else if *auto_scale {
-                log::info!("Auto scaling enabled");
-                if let Some(max_bounds) = get_mqtt_auto_scaled_plot_bounds(mqtt_plots) {
-                    plot_ui.set_plot_bounds(max_bounds);
-                }
+        }
+        let resp = plot_ui.response();
+        if plot_ui.response().double_clicked() || reset_plot_bounds {
+            log::info!("Auto scaling re-enabled");
+            *auto_scale = true;
+            if let Some(max_bounds) = get_mqtt_auto_scaled_plot_bounds(mqtt_plots) {
+                plot_ui.set_plot_bounds(max_bounds);
             }
-            click_delta.ui(plot_ui, PlotType::Hundreds);
-            let (x_lower, x_higher) = plot_util::extended_x_plot_bound(plot_ui.plot_bounds(), 0.1);
-            for mp in mqtt_plots {
-                if mp.data.len() < 2 {
-                    // We don't plot less than two points. It's mostly because when the
-                    // plotting starts, the auto-bounds causes a crash due to auto sizing
-                    // a plot to 1 point and triggering an assert in egui_plot that the
-                    // height and width of the bounds is greater than 0.0
-                    continue;
+        } else if resp.clicked() {
+            *auto_scale = false;
+            log::info!("Auto scaling DISABLED!");
+            if plot_ui.ctx().input(|i| i.modifiers.shift) {
+                if let Some(pointer_coordinate) = plot_ui.pointer_coordinate() {
+                    click_delta.set_next_click(pointer_coordinate, PlotType::Hundreds);
                 }
-                plot_util::plot_raw_mqtt(
-                    plot_ui,
-                    &mp.topic,
-                    &mp.data,
-                    line_width,
-                    (x_lower, x_higher),
-                );
+            } else {
+                click_delta.reset();
             }
-        });
-    }
+        } else if resp.is_pointer_button_down_on() {
+            log::info!("Auto scaling DISABLED!");
+            *auto_scale = false;
+        } else if *auto_scale {
+            log::info!("Auto scaling enabled");
+            if let Some(max_bounds) = get_mqtt_auto_scaled_plot_bounds(mqtt_plots) {
+                plot_ui.set_plot_bounds(max_bounds);
+            }
+        }
+        click_delta.ui(plot_ui, PlotType::Hundreds);
+        let (x_lower, x_higher) = plot_util::extended_x_plot_bound(plot_ui.plot_bounds(), 0.1);
+        for mp in mqtt_plots {
+            if mp.data.len() < 2 {
+                // We don't plot less than two points. It's mostly because when the
+                // plotting starts, the auto-bounds causes a crash due to auto sizing
+                // a plot to 1 point and triggering an assert in egui_plot that the
+                // height and width of the bounds is greater than 0.0
+                continue;
+            }
+            plot_util::plot_raw_mqtt(
+                plot_ui,
+                &mp.topic,
+                &mp.data,
+                line_width,
+                (x_lower, x_higher),
+            );
+        }
+    });
 }
 
 fn get_mqtt_auto_scaled_plot_bounds(mqtt_plots: &[MqttData]) -> Option<PlotBounds> {
@@ -347,7 +384,7 @@ fn build_plot_ui<'a>(
         .label_formatter(crate::util::format_label_ns)
         .link_axis(link_group, Vec2b::new(axis_config.link_x(), false))
         .link_cursor(link_group, [axis_config.link_cursor_x(), false])
-        .y_axis_min_width(50.0) // Adds enough margin for 5-digits
+        .y_axis_min_width(60.0) // Adds enough margin for 5-digits
         .allow_boxed_zoom(true)
         .allow_zoom(false) // Manually implemented
         .allow_scroll(true)
