256751e7c8da42c82e53537789c5f02e79d25569
Marc Beck König <mbkj@tutamail.com>
2025-05-10 19:51:53 +0200

    use mimalloc as the global allocator

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc; // Much faster allocator, can give 20% speedups
+
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result {
