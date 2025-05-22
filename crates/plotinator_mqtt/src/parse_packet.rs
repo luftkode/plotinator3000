11b9ec06f0160fb4d60686bf57cca7abb21e7c55
Marc Beck König <mbkj@tutamail.com>
2025-05-22 13:45:05 +0200

    update pilot display MQTT topics

--- crates/plotinator_mqtt/src/parse_packet.rs
+++ crates/plotinator_mqtt/src/parse_packet.rs
@@ -1,7 +1,7 @@
 use std::str::FromStr as _;
 
-use known_topic::KnownTopic;
 use crate::data::listener::{MqttData, MqttTopicData};
+use known_topic::KnownTopic;
 
 pub mod known_topic;
 
@@ -37,7 +37,6 @@ fn parse_unknown_topic(topic: &str, payload: &str) -> Option<MqttData> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use serde_json::json;
 
     #[test]
     fn test_enum_parse_packet() {
@@ -45,27 +44,6 @@ mod tests {
         let e = KnownTopic::from_str(&s).unwrap();
         let payload = r#"{ "value": 40 }"#;
         let p = e.parse_packet(payload).unwrap();
-        dbg!(p);
-    }
-
-    #[test]
-    fn test_parse_pilot_display_speed_packet() {
-        let s = "speed".to_owned();
-        let e = KnownTopic::from_str(&s).unwrap();
-        let payload = json!({
-            "Speed": "12.433",
-        })
-        .to_string();
-        let p = e.parse_packet(&payload).unwrap();
-        dbg!(p);
-    }
-
-    #[test]
-    fn test_parse_pilot_display_closest_line() {
-        let t = "closest_line";
-        let known_topic = KnownTopic::from_str(t).unwrap();
-        let payload = r#"{"id": 12, "flight_line": "L501100", "distance": 1.84167211, "mode": "automatic", "filename": "20231023_Bremervoerde_Combined_300_NS_32N.kml"}"#;
-        let p = known_topic.parse_packet(payload).unwrap();
         dbg!(p);
     }
 }
