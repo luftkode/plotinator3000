79d0ac10d8aff8b1c139dc2db0de30edbb086849
Marc Beck König <mbkj@tutamail.com>
2025-06-03 13:13:42 +0200

    add trait for HDF5 file implementations

---
+++ crates/plotinator-log-if/src/hdf5.rs
@@ -0,0 +1,9 @@
+use std::{io, path::Path};
+
+use crate::prelude::Plotable;
+
+/// A given HDF5 file should implement this
+pub trait SkytemHdf5: Plotable + Sized {
+    /// Take a path to an HDF5 file and parse it
+    fn from_path(path: impl AsRef<Path>) -> io::Result<Self>;
+}
