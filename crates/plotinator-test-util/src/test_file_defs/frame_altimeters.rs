329c1762f8fa3ea26303414a6a5de9232d56cd79
Marc BK <72197092+CramBL@users.noreply.github.com>
2025-06-30 14:12:40 +0200

    frame-altimeter support (#264)
    * frame-altimeter support
    
    * refactor HDF5 utility

---
+++ crates/plotinator-test-util/src/test_file_defs/frame_altimeters.rs
@@ -0,0 +1,10 @@
+use paste::paste;
+use std::path::PathBuf;
+
+macro_rules! define_frame_altimeters_h5 {
+    ($name:ident, $path:expr) => {
+        define_binary_test_file!($name, concat!("frame_altimeters/", $path));
+    };
+}
+
+define_frame_altimeters_h5!(frame_altimeters, "20250613_092324_frame-altimeters.h5");
