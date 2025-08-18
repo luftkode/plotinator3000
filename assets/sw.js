385f88d5d2c91f05ca0633f0ca1c5bb581da8eb3
Marc Beck König <mbkj@tutamail.com>
2025-08-18 19:15:47 +0200

    only use cached plotinator PWA if fetch fails

--- assets/sw.js
+++ assets/sw.js
@@ -1,25 +1,37 @@
-var cacheName = 'plotinator3000-pwa';
+var cacheName = "plotinator3000-pwa";
 var filesToCache = [
-  './',
-  './index.html',
-  './plotinator3000.js',
-  './plotinator3000_bg.wasm',
+    "./",
+    "./index.html",
+    "./plotinator3000.js",
+    "./plotinator3000_bg.wasm",
 ];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
+self.addEventListener("install", function (e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll(filesToCache);
+        }),
+    );
 });
 
-/* Serve cached content when offline */
-self.addEventListener('fetch', function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (response) {
-      return response || fetch(e.request);
-    })
-  );
+/* Always try to fetch first, fall back to cache if fetch fails */
+self.addEventListener("fetch", function (e) {
+    e.respondWith(
+        fetch(e.request)
+            .then(function (response) {
+                // If fetch succeeds, clone and cache the response for future use
+                if (response && response.status === 200) {
+                    var responseToCache = response.clone();
+                    caches.open(cacheName).then(function (cache) {
+                        cache.put(e.request, responseToCache);
+                    });
+                }
+                return response;
+            })
+            .catch(function () {
+                // If fetch fails, try to get from cache
+                return caches.match(e.request);
+            }),
+    );
 });
