07318f323b33ad4ec809c4c619b5a57bf0a9eeef
Marc Beck König <mbkj@tutamail.com>
2025-09-12 14:18:48 +0200

    add txt to dialog file filter

--- crates/plotinator-file-io/src/file_dialog.rs
+++ crates/plotinator-file-io/src/file_dialog.rs
@@ -6,5 +6,5 @@ pub mod web;
 /// Used for file dialog filters
 pub const FILE_FILTER_NAME: &str = "Known Logs and Plotinator3000 files";
 pub const FILE_FILTER_EXTENSIONS: &[&str] = &[
-    "p3k", "sps", "h5", "hdf5", "hdf", "bin", "log", "zip", "csv",
+    "p3k", "sps", "h5", "hdf5", "hdf", "bin", "log", "zip", "csv", "txt",
 ];
