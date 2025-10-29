a5be986feb00928bb1b600da7439a24249f95b09
Marc Beck König <mbkj@tutamail.com>
2025-10-29 12:54:13 +0100

    move wasp200 under the module njord_altimeter

--- crates/plotinator-hdf5/src/lib.rs
+++ crates/plotinator-hdf5/src/lib.rs
@@ -2,14 +2,23 @@ plotinator_macros::non_wasm_modules!(
     pub(crate) mod util;
     pub(crate) mod stream_descriptor;
     pub mod bifrost;
-    pub mod wasp200;
     pub mod frame_altimeters;
     pub mod frame_inclinometers;
     pub mod frame_magnetometer;
     pub mod frame_gps;
     pub mod njord_ins;
+    pub mod njord_altimeter;
     pub mod tsc;
 );
+
+pub use bifrost::BifrostLoopCurrent;
+pub use frame_altimeters::FrameAltimeters;
+pub use frame_gps::FrameGps;
+pub use frame_inclinometers::FrameInclinometers;
+pub use frame_magnetometer::FrameMagnetometer;
+pub use njord_altimeter::wasp200;
+pub use njord_ins::NjordIns;
+pub use tsc::Tsc;
 
 // File extensions we recognize as hdf5 files.
 const POSSIBLE_HDF5_EXTENSIONS_CASE_INSENSITIVE: [&str; 3] = ["h5", "hdf5", "hdf"];
---
+++ crates/plotinator-hdf5/src/njord_altimeter.rs
@@ -0,0 +1,1 @@
+pub mod wasp200;
